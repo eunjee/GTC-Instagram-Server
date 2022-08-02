4f1d9d9fefbda7d342c18ae6c05b1b65ac69970b
eunjee <rira9817@gmail.com>
2022-08-02 20:09:09 +0900

    프로젝트 생성

---
+++ GTC-instagram/src/main/java/com/beads/GTCinstagram/GtcInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.beads.GTCinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtcInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtcInstagramApplication.class, args);
+	}
+
+}
---
+++ GTC-instagram/src/test/java/com/beads/GTCinstagram/GtcInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beads.GTCinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtcInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
